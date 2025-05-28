4de4f549ad9011c524da3fe705d84c478103e897
Hollow0023 <haruto.izumi0313@gmail.com>
2025-05-28 11:15:28 +0900

    test END

--- PreSotuken/src/main/java/com/order/controller/LoginController.java
+++ PreSotuken/src/main/java/com/order/controller/LoginController.java
@@ -27,7 +27,6 @@ public class LoginController {
         Cookie[] cookies = request.getCookies();
         Integer storeId = null;
         String storeName = null;
-        System.out.println("tes2t3");
 
         if (cookies != null) {
             for (Cookie cookie : cookies) {
